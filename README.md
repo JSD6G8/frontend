852e94645f92ff5e9231cdd87eeb790de8e68c09
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-10 09:43:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JSD6 Group 8 Project
+
+- React framework
+- Tailwind CSS with daisyUI
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Bang Kaew</h1>
+    </>
+  )
+}
+
+export default App
