62d01152ef592b7bb9e10db681daeeb048fda927
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-11 14:15:00 +0700

    finished more card styling

--- src/components/ListedCard.jsx
+++ src/components/ListedCard.jsx
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from "react";
+import Layout from "../Layout";
 
 function ListedCard() {
   return (
     <>
       {/* base card */}
-      <div className="flex flex-row h-32 m-3 shadow-xl rounded-l-xl rounded-r-xl">
+      <div className="flex flex-row h-1/4 m-3 shadow-xl rounded-l-xl rounded-r-xl">
         {/* TODO bg as a variable? */}
-        {/* TODO emofi as a variable */}
-        <div className="bg-power w-32 rounded-l-xl">
+        {/* TODO emoji as a variable */}
+        <div id="activities-baro" className="bg-power w-32 rounded-l-xl">
           <div className="text-7xl">🏊🏼</div>
         </div>
         {/* activity info small */}
         <div className=" bg-base-100 flex-grow flex flex-col justify-evenly rounded-r-xl">
-          <div className="bg-error mt-4 mx-4">
+          <div id="summary-description" className="mt-4 mb-2 mx-4">
             <div>
-              <h1 className="font-extrabold text-2xl">Morning Run</h1>
+              <h1 className="font-extrabold text-xl">Morning Run</h1>
             </div>
             <div>
-              <p className="font-thin">1 Hour 15 mins</p>
+              <p className="font-light text-sm">1 Hour 15 mins</p>
             </div>
           </div>
-          <div className="flex justify-around w-32 bg-fuchsia-300 mx-2 self-end">
-            <p className="font-light text-sm">25/1/2024</p>
-            <p className="font-thin text-sm">at</p>
-            <p className="font-light text-sm">11:25</p>
+          <div
+            id="brief-dateAndTime"
+            className="flex justify-evenly w-32 mb-1 self-end"
+          >
+            <p className="font-thin text-xs">25/1/2024</p>
+            <p className="font-extralight text-slate-400 text-xs italic">at</p>
+            <p className="font-thin text-xs">11:25</p>
           </div>
         </div>
       </div>
