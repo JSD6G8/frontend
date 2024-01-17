724452420ed736677cf61f2b56fa6ea6b508ab1b
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-17 13:54:47 +0700

    Layout.jsx: add hard-coded login state

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,17 +1,29 @@
 /* eslint-disable react/prop-types */
-import { NavGuest, NavLoggedInMobile, NavLoggedInDesktop } from "./components/Nav";
+import {
+  NavGuest,
+  NavLoggedInMobile,
+  NavLoggedInDesktop,
+} from "./components/Nav";
+import { useState } from "react";
 
 // TODO: Add dev login toggle
 // TODO: Make nav fixed position
 
 function Layout({ children }) {
-    return (
-        <div>
-            <NavLoggedInMobile />
-            <NavLoggedInDesktop />
-            {children}
-        </div>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <div>
+      <div className={isLoggedIn ? "hidden" : ""}>
+        <NavGuest />
+      </div>
+      <div className={isLoggedIn ? "" : "hidden"}>
+        <NavLoggedInMobile />
+        <NavLoggedInDesktop />
+      </div>
+      {children}
+    </div>
+  );
 }
 
 export default Layout;
