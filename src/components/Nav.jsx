617e5da0b27c0c6e99efea45d56471ff8eef7584
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-06 11:05:30 +0700

    style: add logo to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,11 @@ function TempLoginToggle({ isLoggedIn, setIsLoggedIn }) {
   return (
     <label className="label flex cursor-pointer flex-row gap-1">
       <span className="label-text">Log-in Toggle</span>
-      <input 
+      <input
         type="checkbox"
         className="toggle"
-        checked={isLoggedIn} 
-        onChange={() => setIsLoggedIn(!isLoggedIn) }
+        checked={isLoggedIn}
+        onChange={() => setIsLoggedIn(!isLoggedIn)}
       />
     </label>
   );
@@ -21,10 +21,13 @@ function NavGuest({ isLoggedIn, setIsLoggedIn }) {
     <nav>
       <div className="navbar bg-white drop-shadow-md">
         <div className="navbar-start">
-          <a className="btn btn-ghost mx-3 text-xl">LogLife</a>
+          <img src="public/logo_black.svg" alt="LogLife logo" />
         </div>
         <div className="navbar-end">
-          <TempLoginToggle isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <TempLoginToggle
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          />
           <button className="btn btn-ghost mx-3 text-base">Login</button>
           <a className="btn btn-primary mx-3 text-base text-white">Sign Up</a>
         </div>
@@ -60,10 +63,10 @@ function NavLoggedInMobile({ isLoggedIn, setIsLoggedIn }) {
             </div>
             <ul
               tabIndex={0}
-              className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-white p-2 shadow"
+              className="menu dropdown-content menu-sm mt-3 w-52 rounded-box bg-white p-2 shadow "
             >
               <li>
-                <a className="py-5 text-base">
+                <a className="py-5 text-base" href="/activities">
                   <i className="fa-solid fa-person-running fa-xl"></i>
                   Activities
                 </a>
@@ -82,10 +85,13 @@ function NavLoggedInMobile({ isLoggedIn, setIsLoggedIn }) {
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">LogLife</a>
+          <img src="public/logo_black.svg" alt="LogLife logo" />
         </div>
         <div className="navbar-end">
-          <TempLoginToggle isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <TempLoginToggle
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          />
           <div className="dropdown dropdown-end mx-3">
             <div
               tabIndex={0}
@@ -117,30 +123,6 @@ function NavLoggedInMobile({ isLoggedIn, setIsLoggedIn }) {
           </div>
         </div>
       </div>
-      {/* <div className=" bg-white drop-shadow-md lg:hidden">
-        <form className="flex items-center p-4">
-          <label className="sr-only">Search</label>
-          <div className="relative w-full">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <svg
-                className="h-5 w-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="simple-search"
-              className=" input input-bordered w-full p-2.5 pl-10"
-              placeholder="Search"
-              required
-            />
-          </div>
-        </form>
-      </div> */}
     </nav>
   );
 }
@@ -150,7 +132,7 @@ function NavLoggedInDesktop({ isLoggedIn, setIsLoggedIn }) {
     <nav>
       <div className="navbar bg-white drop-shadow-md max-lg:hidden">
         <div className="navbar-start">
-          <a className="m-3 text-xl">LogLife</a>
+          <img src="public/logo_black.svg" alt="LogLife logo" />
           <div>
             <ul className="menu menu-horizontal">
               <li>
@@ -172,35 +154,10 @@ function NavLoggedInDesktop({ isLoggedIn, setIsLoggedIn }) {
           </div>
         </div>
         <div className="navbar-end">
-          {/* <div className="w-80">
-            <form className="flex items-center">
-              <label className="sr-only">Search</label>
-              <div className="relative w-full">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-gray-500 dark:text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  id="simple-search"
-                  className=" input input-bordered w-full p-2.5 pl-10"
-                  placeholder="Search"
-                  required
-                />
-              </div>
-            </form>
-          </div> */}
-          <TempLoginToggle isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <TempLoginToggle
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          />
           <div className="dropdown dropdown-end mx-3">
             <div
               tabIndex={0}
