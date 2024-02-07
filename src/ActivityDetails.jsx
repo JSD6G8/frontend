6d347efd56a8c086f5a99ec8c07ccd8a54388f41
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-07 23:25:35 +0700

    style: add rough mobile layout for ActivityDetails

--- src/ActivityDetails.jsx
+++ src/ActivityDetails.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
+import Layout from "./Layout";
+
 function ActivitiesDetails() {
   const activityId = useParams().id;
   const [activity, setActivity] = useState(null);
@@ -26,23 +28,26 @@ function ActivitiesDetails() {
   }, [activityId, navigate]);
 
   return (
-    <main>
-      <h1>Activities Details</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <h2>{activity.title}</h2>
-          <p>{activity.description}</p>
-          <p>{activity.type}</p>
-          <p>{activity.startTime}</p>
-          <p>{activity.endTime}</p>
-          <p>{activity.date}</p>
-          <p>{activity.duration}</p>
-          <p>{activity.barometer}</p>
-        </div>
-      )}
-    </main>
+    <Layout>
+      <main className="container mx-auto flex max-w-lg flex-col items-center px-2 py-2 md:mt-4 md:rounded-xl md:bg-white md:drop-shadow-md lg:px-4">
+        {loading ? (
+          <span className="loading loading-spinner text-primary"></span>
+        ) : (
+          <>
+            <h1>Activities Details</h1>
+            <h2>{activity.title}</h2>
+            <p>{activity.description}</p>
+            <p>{activity.type}</p>
+            <p>{activity.startTime}</p>
+            <p>{activity.endTime}</p>
+            <p>{activity.date}</p>
+            <p>{activity.duration.hour}</p>
+            <p>{activity.duration.minute}</p>
+            <p>{activity.barometer}</p>
+          </>
+        )}
+      </main>
+    </Layout>
   );
 }
 
