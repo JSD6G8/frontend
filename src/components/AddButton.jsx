f2ee531f01c710d84c5050247509ea4c50a293f1
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-02-10 15:51:26 +0700

    style: Small fix on Add activity button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,7 +1,7 @@
 function AddActivityBtn() {
   return (
     <>
-      <div className="fixed bottom-2 right-2 xl:left-3/4 md:hidden">
+      <div className="fixed bottom-2 right-2 md:hidden xl:left-3/4">
         <a href="/activities/create">
           <img
             className="h-[5rem] w-[5rem] transition duration-300 ease-in-out hover:scale-110"
@@ -16,12 +16,16 @@ function AddActivityBtn() {
 
 export function AddButton() {
   return (
-    <div className="rounded-md bg-[#004AAD] px-2 text-[1rem] font-medium tracking-wide text-white md:px-[6px] md:py-[1px] hover:bg-[#1357B8]">
+    <div className="btn bg-[#004AAD] px-2 text-[1rem] font-medium tracking-wide text-white hover:bg-[#1357B8] md:px-[6px] md:py-[1px]">
       <a href="/activities/create">
-        <button className="flex">
-          <p>+</p>
-          <p className="hidden md:inline-block">Activity</p>
-        </button>
+        <div className="flex">
+          <span className="material-symbols-outlined text-4xl font-bold md:self-center">
+            add
+          </span>
+          <p className="hidden self-center text-xl font-semibold md:inline-block md:p-2">
+            Activity
+          </p>
+        </div>
       </a>
     </div>
   );
