6fe6d283a827f95895a8eeacd254dbf77e85c5eb
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-14 11:31:10 +0700

    chore: change home routes for protected and unprotected routes

--- src/main.jsx
+++ src/main.jsx
@@ -25,10 +25,6 @@ const Routes = () => {
 
   const routesForPublic = [
     {
-      path: "/",
-      element: <App />,
-    },
-    {
       path: "/aboutus",
       element: <AboutUs />,
     }
@@ -66,6 +62,10 @@ const Routes = () => {
       path: "/",
       element: <ProtectedRoute />, // Wrap the entire app in a protected route
       children: [
+        {
+          path: "/",
+          element: <ActivityList />,
+        },
         {
           path: "/activities",
           element: <ActivityList />,
